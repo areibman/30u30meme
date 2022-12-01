ac0471a3b6fc91a48657ad1b2d11f423b862b14e
reibs <areibman@gmail.com>
2022-12-01 05:29:31 +0000

    added google analytics and instructions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-6T68C17J51"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-6T68C17J51');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
